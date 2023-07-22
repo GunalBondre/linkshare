04fae09e947d9e86ac84ee8187e3d96eaf02ef60
gunal bondre <bondre.gunal@gmail.com>
2023-07-22 17:05:26 +0530

    login-registration

---
+++ frontend/src/pages/register/Register.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import './register.scss';
+
+const Register: FC = () => {
+	return (
+		<div className='wrapper flex-center'>
+			<div className='form flex-center'>
+				<div className='form__leftSection'></div>
+				<div className='form__rightSection'>
+					<h3>Signup for bio link</h3>
+					<form action=''></form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
